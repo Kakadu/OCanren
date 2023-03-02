03b0ab4117c406f213bcf47bfd6d28ae383a4a29
kakadu <kakadu@pm.me>
2023-03-02 16:19:24 +0300

    Use 'dune exec regression/test001.exe | nl -ba'
    Signed-off-by: kakadu <kakadu@pm.me>

--- regression/test001.ml
+++ regression/test001.ml
@@ -33,6 +33,16 @@ let show_int       = show(int)
 let show_int_list   = show(List.ground) (show int)
 let show_intl_list  = show(List.logic ) (show(logic) (show int))
 
+let (===) a b =
+  let reif = Std.Pair.reify (Std.List.reify OCanren.reify) (Std.List.reify OCanren.reify) in
+  (debug_var (Std.pair a b) (Fun.flip reif) (function
+    | [ Value (x, y) ] ->
+      Format.printf "unify `%s` and `%s`\n%!" (show_intl_list x) (show_intl_list y);
+      success
+    | _ -> assert false
+  )) &&& (OCanren.unify a b)
+
+
 let rec appendo a b ab =
   conde
     [ ((a === nil ()) &&& (b === ab))
@@ -51,6 +61,21 @@ let rec reverso a b =
         (defer (reverso t a'))
     ]
 
+let run_exn eta = run_r (Std.List.prj_exn OCanren.prj_exn) eta
+let __ _ =
+  run_exn show_int_list  1  q qh (REPR (fun q   -> appendo q (ilist [3; 4]) (ilist [1; 2; 3; 4])   ));
+  ()
+
+let __ () =
+  run_exn show_int_list (-1)  qr qrh (REPR (fun q r  ->
+    appendo q r (ilist [1; 2; 3; 4])   ));
+  ()
+let () =
+  run_exn show_int_list (-1)  q qh (REPR (fun q   ->
+    reverso q (ilist [1])   ));
+  ()
+
+(*
 let run_exn eta = run_r (Std.List.prj_exn OCanren.prj_exn) eta
 let _ =
   run_exn show_int_list  1  q qh (REPR (fun q   -> q === !!1 % q));
@@ -78,4 +103,4 @@ let _withFree =
   runL          3  q  qh (REPR (fun q   -> reverso q q                                  ));
   runL         10  q  qh (REPR (fun q   -> reverso q q                                  ));
   runL          1 qr qrh (REPR (fun q r -> two_vars q r                                 ));
-  ()
+  () *)
