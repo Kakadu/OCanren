ddcc0fe6364a862a3ce3b9ebf94b3c02c72ce5bd
Dmitrii Kosarev a.k.a. Kakadu <Kakadu@pm.me>
2025-12-17 18:16:44 +0300

    Update test
    Signed-off-by: Dmitrii Kosarev a.k.a. Kakadu <Kakadu@pm.me>

--- camlp5/1.t
+++ camlp5/1.t
@@ -1,5 +1,6 @@
-  $ ls
-  $ ls ../ppx
+$ ls
+$ ls ../ppx
 $ ocamlfind c -c test006.ml -pp ../ppx/pp_distrib.exe -verbose
   $ cat test006.ml
   $ ../ppx/pp_distrib.exe -new-typenames test006.ml
+$ ocamlfind c -pp '../ppx/pp_distrib.exe -new-typenames' -package OCanren -rectypes test006.ml
--- camlp5/test006.ml
+++ camlp5/test006.ml
@@ -1,9 +1,9 @@
-module _ = struct
 
-  [%%ocanren_inject
-    type aa = A of bb
-    and bb = aa GT.list
-  ]
-end
+[%%ocanren_inject
+  type aa = A of bb
+  and bb = aa GT.list
+  (* [@@deriving gt ~options:{gmap}] *)
+]
 
-let () = print_endline "test007"
+
+(* let () = print_endline "test007" *)
