cbaa30509ee046838e777f34dff20c096c8a0784
Kakadu <kakadu.hafanana@gmail.com>
2026-08-27 13:25:09 +0300

    Forgottent changes in src/core/Core.mli
    Signed-off-by: Kakadu <kakadu.hafanana@gmail.com>

--- src/core/Core.mli
+++ src/core/Core.mli
@@ -286,8 +286,8 @@ val trace_diseq: goal
 IFDEF NON_ABSTRACT_GOAL THEN
 val reify_in_state: State.t -> ('a, 'b) Reifier.t -> 'a -> 'b
 
-val is_ground : 'a ilogic -> State.t -> (bool -> unit) -> unit
+val is_ground : ('a, 'b) injected -> State.t -> (bool -> unit) -> unit
 
-val is_ground_bool : bool ilogic -> State.t -> onvar:(unit->unit) -> on_ground:(bool -> unit) -> unit
+val is_ground_bool : (bool, bool logic) injected -> State.t -> onvar:(unit->unit) -> on_ground:(bool -> unit) -> unit
 
 END
